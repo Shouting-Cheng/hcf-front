00dc2059a8e63a15d4c26db724617a2a773fb1c8
陈志鹏 <zhipeng.chen01@hand-china.com>
2018-10-24 20:16:16 +0800

    法人实体迁移

--- src/common/router.js
+++ src/common/router.js
@@ -930,6 +930,30 @@ export const getRouterData = app => {
       ),
       name: '编辑汇率',
       parent:'/admin-setting/currency-setting'
+    },
+    //实体法人
+    '/enterprise-manage/legal-person' : {
+      component: dynamicWrapper(app, [], () =>
+        import('containers/enterprise-manage/legal-person/legal-person.js')
+      ),
+      name: 'legal-person',
+      parent:'/enterprise-manage'
+    },
+    //实体法人详情
+    '/enterprise-manage/legal-person/legal-person-detail/:legalPersonOID/:legalPersonID' : {
+      component: dynamicWrapper(app, [], () =>
+        import('containers/enterprise-manage/legal-person/legal-person-detail.js')
+      ),
+      name: 'legal-person-detail',
+      parent:'/enterprise-manage/legal-person'
+    },
+    //编辑实体法人
+    '/enterprise-manage/legal-person/new-legal-person/:legalPersonOID/:legalPersonID' : {
+      component: dynamicWrapper(app, [], () =>
+        import('containers/enterprise-manage/legal-person/new-legal-person.js')
+      ),
+      name: 'new-legal-person',
+      parent:'/enterprise-manage/legal-person'
     }
 
     // '/user/:id': {
