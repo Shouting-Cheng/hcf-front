15b22c722646bd3c9ea028ba23e8fea042157c8e
成寿庭 <shouting.cheng@hand-china.com>
2018-11-07 17:32:07 +0800

    一次查询中国的所有省和市

--- src/containers/basic-data/bank-definition/bank-definition.js
+++ src/containers/basic-data/bank-definition/bank-definition.js
@@ -436,12 +436,16 @@ class BankDefinition extends React.Component {
       page: 0,
       size: 1000,
     };
-    BSService.getStates(params).then(response => {
+    BSService.getStatesAndCitys(params).then(response => {
       let children = response.data.map(item => {
         item.label = item.state;
         item.value = item.code;
-        item.children = [];
-        this.getCityByCode(item.code);
+        item.children.map(city => {
+          city.label = city.city;
+          city.value = city.code;
+        })
+        //item.children = [];
+        //this.getCityByCode(item.code);
         return item;
       });
       this.setChinaState(children);
@@ -781,6 +785,7 @@ class BankDefinition extends React.Component {
           title={slideFrame.title}
           show={slideFrame.visible}
           onClose={() => this.setState({ slideFrame: { visible: false } })}>
+          {console.log(slideFrame.params)}
           <CreateOrUpdateBank
               params={slideFrame.params}
               onClose={this.handleCloseSlide}/>
