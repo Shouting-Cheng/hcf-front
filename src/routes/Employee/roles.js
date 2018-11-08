9535fd5a97b9962ee3accbbb486acbf0c5fc88d5
rookie <947405755@qq.com>
2018-11-08 16:02:35 +0800

    hec-688 员工管理-分配角色-缓存问题

--- src/routes/Employee/roles.js
+++ src/routes/Employee/roles.js
@@ -83,7 +83,7 @@ class SelectRoles extends React.Component {
 
       service.getRoles(params).then(res => {
         let ids = res.map(item => item.id);
-        this.setState({ selectedRowKeys: ids, defaultIds: ids });
+        this.setState({ dataSource: res, selectedRowKeys: ids, defaultIds: ids });
       });
     }
   }
