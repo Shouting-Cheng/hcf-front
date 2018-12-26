731375e69d25d42e6e7ad5c1a0ca8cbfeb1f8ee1
17364 <debao.fu@hand-china.com>
2018-12-26 16:52:24 +0800

    fixed bug

--- src/config/config.js
+++ src/config/config.js
@@ -14,7 +14,7 @@ const config = {
   prePaymentUrl: '/prepayment',
   // vendorUrl:`http://116.228.77.183:25297/vendor`, //供应商url    +  /vendor-info-service
   localUrl: `http://localhost:9998`,
-  locationUrl: `/location`,
+  locationUrl: `/`,
   mapUrl: '/mapUrl',
   ssoUrl: 'http://139.224.2.45:11059',
   wsUrl: 'ws://http://106.15.26.10:10080',
@@ -22,7 +22,7 @@ const config = {
   //baseUrl:'http://116.228.77.183:25297',
   jobUrl: '/job',
   vendorUrl: '/supplier',
-  brmsUrl: '/brms',
+  brmsUrl: '/',
   // Settings configured here will be merged into the final config object.
   mapKey: 'E5XBZ-LWVWJ-2TUFJ-F73PP-VS5LS-W3FUM',
   expenseUrl: '/expense',
