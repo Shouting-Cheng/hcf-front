f0cdd228168278fc21110226979b26392dd04e88
yuqin.sheng <yuqin.sheng@hand-china.com>
2019-01-07 15:39:32 +0800

    '责任中心对接'

--- src/containers/setting/responsibility-center/responsibility-service.js
+++ src/containers/setting/responsibility-center/responsibility-service.js
@@ -9,5 +9,21 @@ export default{
     saveResponsibility(params){
         let url = `${config.baseUrl}/api/responsibilityCenter/insertOrUpdate`;
         return httpFetch.post(url, params);
-    }
+    },
+    /**
+     * 
+     * 保存新建责任中心组
+     */
+    saveResponsibilityGroup(params){
+        let url = `${config.baseUrl}/api/responsibilityCenter/group/insertOrUpdate`;
+        return httpFetch.post(url, params);
+    },
+    deleteResponsibility(id){
+        let url=`${config.baseUrl}/api/responsibilityCenter/delete/${id}`;
+        return httpFetch.delete(url)
+    },
+    deleteResponsibilityGroup(id){
+        let url=`${config.baseUrl}/api/responsibilityCenter/group/delete/${id}`;
+        return httpFetch.delete(url)
+    },
 }
